2ef13d7002294ac23bae4e8c3091550237e9b742
Launacloud <109642716+Launacloud@users.noreply.github.com>
2024-06-05 22:59:35 -0300

    Update send_rss_to_telegram.py

--- send_rss_to_telegram.py
+++ send_rss_to_telegram.py
@@ -1,3 +1,64 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+import feedparser
+import json
+
+# Load environment variables
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+RSS_FEED_URL = os.getenv('RSS_FEED_URL')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+# Cache file path
+CACHE_FILE_PATH = 'feed_cache.json'
+
+# Function to load cache from the file
+def load_cache():
+    if os.path.exists(CACHE_FILE_PATH):
+        with open(CACHE_FILE_PATH, 'r') as file:
+            cache = json.load(file)
+            print(f"Cache loaded from file: {CACHE_FILE_PATH}")
+            return cache
+    else:
+        print("No cache file found. Starting with an empty cache.")
+        return {}
+
+# Function to save cache to the file
+def save_cache(cache):
+    with open(CACHE_FILE_PATH, 'w') as file:
+        json.dump(cache, file, indent=4)
+    print(f"Cache saved to file: {CACHE_FILE_PATH}")
+
+# Function to send a message to a Telegram chat
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': message,
+        'parse_mode': 'HTML',
+        'disable_web_page_preview': 'false'  # Enable link previews
+    }
+    response = requests.post(url, data=payload)
+    print(f"Response status code: {response.status_code}")
+    print(f"Response text: {response.text}")
+    if response.status_code != 200:
+        raise Exception(f"Error sending message: {response.text}")
+
+# Function to fetch RSS feed with conditional requests
+def fetch_rss_feed(etag=None, modified=None):
+    headers = {}
+    if etag:
+        headers['If-None-Match'] = etag
+    if modified:
+        headers['If-Modified-Since'] = modified
+    
+    response = requests.get(RSS_FEED_URL, headers=headers)
+    response.raise_for_status()
+    
+    feed = feedparser.parse(response.content)
+    feed.status = response.status_code
+    return feed
+
 # Function to send RSS feed items to Telegram
 def send_rss_to_telegram():
     cache = load_cache()
@@ -11,7 +72,7 @@ def send_rss_to_telegram():
     print(f"Loading feed with etag: {etag} and modified: {modified}")
     feed = fetch_rss_feed(etag=etag, modified=modified)
 
-    if feed.status == 304.:
+    if feed.status == 304:
         print("No new entries.")
         return
 
@@ -67,3 +128,10 @@ def send_rss_to_telegram():
 
     # Save cache if there are new entries
     save_cache(cache)
+
+# Main function
+def main():
+    send_rss_to_telegram()
+
+if __name__ == "__main__":
+    main()
